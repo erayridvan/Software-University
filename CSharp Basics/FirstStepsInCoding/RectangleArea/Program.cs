6ef13a7f33fbc4bf8af424d51820e562ab8337d8
erayridvan <52295311+erayridvan@users.noreply.github.com>
2019-07-01 20:23:24 +0100

    C#

---
+++ CSharp Basics/FirstStepsInCoding/RectangleArea/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            int rectangleArea = a * b;
+
+            Console.WriteLine(rectangleArea);
+        }
+    }
+}
