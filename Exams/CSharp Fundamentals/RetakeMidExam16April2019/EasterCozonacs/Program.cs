d32e7392101bf1d5a6b81ef7d4df1dca1d58c87b
erayridvan <52295311+erayridvan@users.noreply.github.com>
2019-07-02 05:34:59 +0100

    C#Fundamentals

---
+++ Exams/CSharp Fundamentals/RetakeMidExam16April2019/EasterCozonacs/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace EasterCozonacs
+{
+    class Program
+    {
+        static void Main()
+        {
+            double budget = double.Parse(Console.ReadLine());
+            double flourPrice = double.Parse(Console.ReadLine());
+            double packOfEggsPrice = flourPrice * 0.75;
+            double milkPrice = flourPrice + (flourPrice * 0.25);
+            double quaterMilk = milkPrice / 4.0;
+            double totalPriceForOneCozonac = packOfEggsPrice + flourPrice + quaterMilk;
+            int colouredEggsCounter = 0;
+            int cozonacCounter = 0;
+
+            while (true)
+            {
+                if (budget< totalPriceForOneCozonac)
+                {
+                    break;
+                }
+
+                cozonacCounter++;
+                if (cozonacCounter % 3==0)
+                {
+                    colouredEggsCounter -= (cozonacCounter - 2);
+                }
+                budget -= totalPriceForOneCozonac;
+                colouredEggsCounter += 3;
+            }
+
+            Console.WriteLine($"You made {cozonacCounter} cozonacs! Now you have {colouredEggsCounter} eggs and {budget:f2}BGN left.");
+        }
+    }
+}
