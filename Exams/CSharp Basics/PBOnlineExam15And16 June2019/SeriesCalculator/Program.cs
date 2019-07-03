aefe71597a651b523bcdf37a252447b0ef90df07
erayridvan <52295311+erayridvan@users.noreply.github.com>
2019-07-03 16:14:11 +0100

    C#

---
+++ Exams/CSharp Basics/PBOnlineExam15And16 June2019/SeriesCalculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SeriesCalculator
+{
+    class Program
+    {
+        static void Main()
+        {
+            string nameOfSeries = Console.ReadLine();
+            int numberOfSeasons = int.Parse(Console.ReadLine());
+            int numberOfEpisodes = int.Parse(Console.ReadLine());
+            double timeForOneEpisode = double.Parse(Console.ReadLine());
+
+            double advertiseTime = 0.20 * timeForOneEpisode;
+            double totalTimeForOneEpisode = advertiseTime + timeForOneEpisode;
+            int specialEpisodeTime = numberOfSeasons * 10;
+            double totalMinutesToWatch = totalTimeForOneEpisode * numberOfEpisodes * numberOfSeasons + specialEpisodeTime;
+
+            Console.WriteLine($"Total time needed to watch the {nameOfSeries} series is {totalMinutesToWatch} minutes.");
+        }
+    }
+}
