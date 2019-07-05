1b2945138d26ba8fb30653902b15ee29bfd1063d
erayridvan <52295311+erayridvan@users.noreply.github.com>
2019-07-05 05:11:40 +0100

    C# Fundamentals

--- CSharp Fundamentals/CSharpFundamentalsAssociativeArraysLab/WordFilter/Program.cs
+++ CSharp Fundamentals/CSharpFundamentalsAssociativeArraysLab/WordFilter/Program.cs
@@ -8,7 +8,15 @@ namespace WordFilter
     {
         static void Main()
         {
+            string[] words = Console.ReadLine()
+                .Split(" ")
+                .Where(w => w.Length % 2 == 0)
+                .ToArray();
 
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
         }
     }
 }
