f8d29c0f84b83d0485f625820a11a4dc2a8ec10b
erayridvan <52295311+erayridvan@users.noreply.github.com>
2019-07-05 06:41:19 +0100

    C# Fundamentals

---
+++ CSharp Fundamentals/CSharpFundAssociativeArraysExercise/AMinerTask/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AMinerTask
+{
+    class Program
+    {
+        static void Main()
+        {
+            var dict = new Dictionary<string, int>();
+            
+            string text = Console.ReadLine();
+            while (text != "stop")
+            {
+                if (!(dict.ContainsKey(text)))
+                {
+                    dict[text] = int.Parse(Console.ReadLine());
+                }
+                else
+                {
+                    dict[text] += int.Parse(Console.ReadLine());
+                }
+                text = Console.ReadLine();
+            }
+
+            foreach (var resource in dict)
+            {
+                Console.WriteLine($"{resource.Key} -> {resource.Value}");
+            }
+        }
+    }
+}
---
+++ CSharp Fundamentals/CSharpFundAssociativeArraysExercise/CountCharsInAString/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountCharsInAString
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<string> words = Console.ReadLine()
+                .Split(" ")
+                .ToList();
+
+            var occurrences = new Dictionary<char, int>();
+
+            for (int i = 0; i < words.Count; i++)
+            {
+                string curruntWord = words[i];
+                for (int k = 0; k < curruntWord.Length; k++)
+                {
+                    char letter = curruntWord[k];
+
+                    if (!(occurrences.ContainsKey(letter)))
+                    {
+                        occurrences[letter] = 0;
+                    }
+                    occurrences[letter]++;
+                }
+            }
+
+            foreach (var letter in occurrences)
+            {
+                Console.WriteLine($"{letter.Key} -> {letter.Value}");
+            }
+        }
+    }
+}
