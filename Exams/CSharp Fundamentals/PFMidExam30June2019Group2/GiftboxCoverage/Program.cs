8f310c610734841bfeea4c88d30f03eaac068399
erayridvan <52295311+erayridvan@users.noreply.github.com>
2019-07-13 14:51:44 +0100

    C# Fundamentals

---
+++ Exams/CSharp Fundamentals/PFMidExam30June2019Group2/GiftboxCoverage/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GiftboxCoverage
+{
+    class Program
+    {
+        static void Main()
+        {
+            double sizeOfSide = double.Parse(Console.ReadLine());
+            int sheets = int.Parse(Console.ReadLine());
+            double sheetArea = double.Parse(Console.ReadLine());
+
+
+            double boxArea = sizeOfSide * sizeOfSide * 6;
+            double totalSheetArea = sheets * sheetArea;
+
+            int sheetsCoverLess = sheets / 3;
+            int LeftSheets = sheets - sheetsCoverLess;
+
+            double totalAreaSheetsCover = (LeftSheets * sheetArea) + (sheetsCoverLess*(0.25*sheetArea));
+            double percentage = totalAreaSheetsCover * 100 / boxArea;
+
+            Console.WriteLine("You can cover {0:f2}% of the box.",percentage);
+        }
+    }
+}
