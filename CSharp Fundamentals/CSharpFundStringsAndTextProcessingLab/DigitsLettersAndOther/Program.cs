718d742eccd3f2936ba34af8d2c54f84ed8a84c5
erayridvan <52295311+erayridvan@users.noreply.github.com>
2019-07-13 07:09:58 +0100

    C# Fundamentals

--- CSharp Fundamentals/CSharpFundStringsAndTextProcessingLab/DigitsLettersAndOther/Program.cs
+++ CSharp Fundamentals/CSharpFundStringsAndTextProcessingLab/DigitsLettersAndOther/Program.cs
@@ -7,7 +7,37 @@ namespace DigitsLettersAndOther
     {
         static void Main()
         {
+            string text = Console.ReadLine();
 
+            string digits = "";
+            string letters = "";
+            string others = "";
+        
+            for (int i = 0; i < text.Length; i++)
+            {
+                char currentSymbol = text[i];
+
+                bool isLetter = char.IsLetter(currentSymbol);
+                bool isDigit = char.IsDigit(currentSymbol);
+
+                if (isLetter == true)
+                {
+                    letters += currentSymbol;
+                }
+                else if (isDigit==true)
+                {
+                    digits += currentSymbol;
+                }
+                else
+                {
+                    others += currentSymbol;
+                }
+            }
+
+
+            Console.WriteLine(digits);
+            Console.WriteLine(letters);
+            Console.WriteLine(others);
         }
     }
 }
