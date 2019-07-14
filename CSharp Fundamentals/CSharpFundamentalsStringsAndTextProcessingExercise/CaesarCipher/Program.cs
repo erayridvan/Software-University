05cd179b9c2076cf25b6764a9a6a374b65515183
erayridvan <52295311+erayridvan@users.noreply.github.com>
2019-07-14 06:43:37 +0100

    C# Fundamentals

--- CSharp Fundamentals/CSharpFundamentalsStringsAndTextProcessingExercise/CaesarCipher/Program.cs
+++ CSharp Fundamentals/CSharpFundamentalsStringsAndTextProcessingExercise/CaesarCipher/Program.cs
@@ -4,9 +4,26 @@ namespace CaesarCipher
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            string text = Console.ReadLine();
+
+            Encryption(text);
+
+        }
+
+        static void Encryption(string text)
+        {
+            string result = "";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char currentSymbol = text[i];
+
+                result += (char)(currentSymbol + 3);
+            }
+
+            Console.WriteLine(result);
         }
     }
 }
