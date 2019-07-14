150edaa24ac55e2e37c2a4a2918569196eddfb2f
erayridvan <52295311+erayridvan@users.noreply.github.com>
2019-07-14 06:30:54 +0100

    C# Fundamentals

---
+++ CSharp Fundamentals/CSharpFundamentalsStringsAndTextProcessingExercise/CharacterMultiplier/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CharacterMultiplier
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] words = Console.ReadLine()
+                .Split(" ")
+                .ToArray();
+
+            string firstWord = words[0];
+            string secondWord = words[1];
+            int totalSum = 0;
+
+            if (firstWord.Length==secondWord.Length)
+            {
+                for (int i = 0; i < firstWord.Length; i++)
+                {
+                    char symbolOne = firstWord[i];
+                    char symbolTwo = secondWord[i];
+
+                    totalSum += (symbolOne * symbolTwo);
+                }
+            }
+            else if (firstWord.Length>secondWord.Length)
+            {
+                for (int i = 0; i < firstWord.Length; i++)
+                {
+                    char symbolOne = firstWord[i];
+                    if (i<secondWord.Length)
+                    {
+                        char symbolTwo = secondWord[i];
+                        totalSum += (symbolOne * symbolTwo);
+                    }
+                    else
+                    {
+                        totalSum += symbolOne;
+                    }
+                }
+            }
+            else if (secondWord.Length>firstWord.Length)
+            {
+                for (int i = 0; i < secondWord.Length; i++)
+                {
+                    char symbolOne = secondWord[i];
+                    if (i < firstWord.Length)
+                    {
+                        char symbolTwo = firstWord[i];
+                        totalSum += (symbolOne * symbolTwo);
+                    }
+                    else
+                    {
+                        totalSum += symbolOne;
+                    }
+                }
+            }
+
+            Console.WriteLine(totalSum);
+        }
+    }
+}
---
+++ CSharp Fundamentals/CSharpFundamentalsStringsAndTextProcessingExercise/ExtractFile/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace ExtractFile
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] text = Console.ReadLine().Split("\\");
+            string[] file = text[text.Length-1].Split(".").ToArray();
+
+            string fileName = file[0];
+            string fileExtension = file[1];
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {fileExtension}");
+        }
+    }
+}
