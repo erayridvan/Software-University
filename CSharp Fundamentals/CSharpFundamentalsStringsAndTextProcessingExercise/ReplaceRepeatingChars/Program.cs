8cd00737436d08c96d86ce97eaafc904f04d5832
erayridvan <52295311+erayridvan@users.noreply.github.com>
2019-07-14 10:53:27 +0100

    C# Fundamentals

---
+++ CSharp Fundamentals/CSharpFundamentalsStringsAndTextProcessingExercise/ReplaceRepeatingChars/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ReplaceRepeatingChars
+{
+    class Program
+    {
+        static void Main()
+        {
+            string text = Console.ReadLine();
+            char symbol = '0';
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char currentSymbol = text[i];
+
+                if (currentSymbol!=symbol)
+                {
+                    Console.Write(currentSymbol);
+                }
+                symbol = currentSymbol;
+            }
+
+            Console.WriteLine();
+
+        }
+    }
+}
