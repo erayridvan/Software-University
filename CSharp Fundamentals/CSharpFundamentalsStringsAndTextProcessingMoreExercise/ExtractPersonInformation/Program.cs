8feaaeca3cb647c34952de63a3fa1112f54614e5
erayridvan <52295311+erayridvan@users.noreply.github.com>
2019-07-15 15:43:54 +0100

    C# Fundamentals

---
+++ CSharp Fundamentals/CSharpFundamentalsStringsAndTextProcessingMoreExercise/ExtractPersonInformation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExtractPersonInformation
+{
+    class Program
+    {
+        static void Main()
+        {
+            string text = "@eray|";
+            char[] symbols = text.ToCharArray();
+            symbols = symbols.SkipWhile(n => n == '@').SkipWhile(l=>l=='|').ToArray();
+            Console.WriteLine(symbols);
+        }
+    }
+}
