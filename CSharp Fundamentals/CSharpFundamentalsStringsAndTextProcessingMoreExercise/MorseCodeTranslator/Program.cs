64a943cce49333c591b965e71b76a73c8cda7494
erayridvan <52295311+erayridvan@users.noreply.github.com>
2019-07-15 16:55:30 +0100

    C# Fundamentals

--- CSharp Fundamentals/CSharpFundamentalsStringsAndTextProcessingMoreExercise/MorseCodeTranslator/Program.cs
+++ CSharp Fundamentals/CSharpFundamentalsStringsAndTextProcessingMoreExercise/MorseCodeTranslator/Program.cs
@@ -1,12 +1,54 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
 
 namespace MorseCodeTranslator
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            string input = Console.ReadLine().Split('|').Trim().ToArray();
+            string output = "";
+            foreach (string word in input)
+            {
+                    if (morseToChar.ContainsValue(word))
+                    {
+                        output += (morseToChar.FirstOrDefault(x => x.Value == word).Key);
+                    }
+            }
+            Console.WriteLine(output);
+
         }
+        static Dictionary<char, string> morseToChar = new Dictionary<char, string>()
+        {
+            {'a', ".-"},
+            {'b', "-..."},
+            {'c', "-.-."},
+            {'d', "-.."},
+            {'e', "."},
+            {'f', "..-."},
+            {'g', "--."},
+            {'h', "...."},
+            {'i', ".."},
+            {'j', ".---"},
+            {'k', "-.-"},
+            {'l', ".-.."},
+            {'m', "--"},
+            {'n', "-."},
+            {'o', "---"},
+            {'p', ".--."},
+            {'q', "--.-"},
+            {'r', ".-."},
+            {'s', "..."},
+            {'t', "-"},
+            {'u', "..-"},
+            {'v', "...-"},
+            {'w', ".--"},
+            {'x', "-..-"},
+            {'y', "-.--"},
+            {'z', "--.."}
+         };
     }
 }
