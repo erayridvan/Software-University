3d687eabdd6de5cc17efa2e0a033c142a613467e
erayridvan <52295311+erayridvan@users.noreply.github.com>
2019-07-15 16:28:02 +0100

    C# Fundamentals

--- CSharp Fundamentals/CSharpFundamentalsStringsAndTextProcessingMoreExercise/AsciiSumator/Program.cs
+++ CSharp Fundamentals/CSharpFundamentalsStringsAndTextProcessingMoreExercise/AsciiSumator/Program.cs
@@ -4,9 +4,45 @@ namespace AsciiSumator
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            char firstSymbol = char.Parse(Console.ReadLine());
+            char secondSymbol = char.Parse(Console.ReadLine());
+            string text = Console.ReadLine();
+
+            string result = "";
+
+            if (firstSymbol>secondSymbol)
+            {
+                for (int i = 0; i < text.Length; i++)
+                {
+                    char currentSymbol = text[i];
+                    if (currentSymbol<firstSymbol && currentSymbol>secondSymbol)
+                    {
+                        result += currentSymbol.ToString();
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < text.Length; i++)
+                {
+                    char currentSymbol = text[i];
+                    if (currentSymbol > firstSymbol && currentSymbol < secondSymbol)
+                    {
+                        result += currentSymbol.ToString();
+                    }
+                }
+            }
+
+            int sum = 0;
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                sum += (int)result[i];
+            }
+
+            Console.WriteLine(sum);
         }
     }
 }
