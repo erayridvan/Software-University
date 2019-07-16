cd953ce189288dbe68f9048e8120126e364dc35e
erayridvan <52295311+erayridvan@users.noreply.github.com>
2019-07-16 06:14:20 +0100

    C# Fundamentals

--- CSharp Fundamentals/CSharpFundamentalsStringsAndTextProcessingMoreExercise/ExtractPersonInformation/Program.cs
+++ CSharp Fundamentals/CSharpFundamentalsStringsAndTextProcessingMoreExercise/ExtractPersonInformation/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace ExtractPersonInformation
 {
@@ -8,7 +9,27 @@ namespace ExtractPersonInformation
     {
         static void Main()
         {
+            public class Program
+        {
+            public static void Main()
+            {
+                int n = int.Parse(Console.ReadLine());
 
+
+                Regex namePattern = new Regex(@"@(?<name>[\S\s]*?)\|");
+                Regex agePattern = new Regex(@"#(?<age>[\S\s]*?)\*");
+
+                for (int i = 0; i < n; i++)
+                {
+                    string input = Console.ReadLine();
+
+                    if (namePattern.IsMatch(input) && agePattern.IsMatch(input))
+                    {
+                        Console.WriteLine($"{namePattern.Match(input).Groups["name"].ToString()} is {agePattern.Match(input).Groups["age"].ToString()} years old.");
+                    }
+                }
+
+            }
         }
     }
 }
