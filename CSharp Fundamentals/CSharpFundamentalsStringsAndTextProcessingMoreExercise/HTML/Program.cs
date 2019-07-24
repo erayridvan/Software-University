64e13f48b03f2ba2b202f83b8b4c48f5c9c50540
erayridvan <52295311+erayridvan@users.noreply.github.com>
2019-07-24 20:02:00 +0100

    C# Fundamentals

--- CSharp Fundamentals/CSharpFundamentalsStringsAndTextProcessingMoreExercise/HTML/Program.cs
+++ CSharp Fundamentals/CSharpFundamentalsStringsAndTextProcessingMoreExercise/HTML/Program.cs
@@ -6,7 +6,10 @@ namespace HTML
     {
         static void Main()
         {
-
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
