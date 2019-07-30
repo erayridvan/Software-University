06ba19d736c858ff9c2328e9b88f7b56f75aa3fc
erayridvan <52295311+erayridvan@users.noreply.github.com>
2019-07-30 18:21:30 +0100

    C# Fundamentals

---
+++ CSharp Fundamentals/CSharpFundRegularExpressionsRegexLab/MatchDates/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MatchDates
+{
+    class Program
+    {
+        static void Main()
+        {
+            var regex = @"[0-9]{2}(\.|-|\/)[A-z][a-z]{2}\1[0-9]{4}";
+
+            var dates = Console.ReadLine();
+
+            MatchCollection matchedDates = Regex.Matches(dates, regex);
+        }
+    }
+}
