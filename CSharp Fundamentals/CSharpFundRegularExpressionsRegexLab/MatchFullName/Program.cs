f821b1a444166001c15c0ff1c7d7dce4ac7c3a6d
erayridvan <52295311+erayridvan@users.noreply.github.com>
2019-07-30 17:36:45 +0100

    C# Fundamentals

--- CSharp Fundamentals/CSharpFundRegularExpressionsRegexLab/MatchFullName/Program.cs
+++ CSharp Fundamentals/CSharpFundRegularExpressionsRegexLab/MatchFullName/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace MatchFullName
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var pattern = new Regex("[A - Z][a - z] + [A - Z][a - z] +");
+
+            string names = Console.ReadLine();
+
+            var matchedNames = pattern.Matches(names);
+
+            foreach (Match name in matchedNames)
+            {
+                Console.WriteLine(name);
+            }
         }
     }
 }
