f889ebc6102c59202f21f50d950c31240d688e4b
erayridvan <52295311+erayridvan@users.noreply.github.com>
2019-08-02 10:55:13 +0100

    C# Fundamentals

---
+++ CSharp Fundamentals/CSharpFundamentalsRegexExercise/Furniture/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Furniture
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<string> boughtFurniter = new List<string>();
+            int totalMoneySpend = 0;
+
+            string pattern = @"([A-za-z]+)<<([0-9]+\.*[0-9]*)!([0-9])";
+            string command = string.Empty;
+
+            var regex = new Regex(@"([A-za-z]+)<<([0-9]+\.*[0-9]*)!([0-9])");
+            while ((command=Console.ReadLine()) != "Purchase")
+            {
+                if (regex.IsMatch(command))
+                {
+                    MatchCollection matches = Regex.Matches(command, pattern);
+                    
+                }
+            }
+        }
+    }
+}
