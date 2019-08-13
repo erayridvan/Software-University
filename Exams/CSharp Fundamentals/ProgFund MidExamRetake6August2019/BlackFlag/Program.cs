48d7bdaba6fabd3ba1e602555690b718648daa23
erayridvan <52295311+erayridvan@users.noreply.github.com>
2019-08-13 18:15:42 +0100

    C# Fundamentals

--- Exams/CSharp Fundamentals/ProgFund MidExamRetake6August2019/BlackFlag/Program.cs
+++ Exams/CSharp Fundamentals/ProgFund MidExamRetake6August2019/BlackFlag/Program.cs
@@ -4,9 +4,42 @@ namespace BlackFlag
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            double days = double.Parse(Console.ReadLine());
+            double dailyPlunder = double.Parse(Console.ReadLine());
+            double expectedPlunder = double.Parse(Console.ReadLine());
+
+            double totalPlunder = 0;
+
+            for (int i = 1; i <= days; i++)
+            {
+                totalPlunder += dailyPlunder;
+
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    totalPlunder += (dailyPlunder * 0.50);
+                    totalPlunder -= (totalPlunder * 0.30);
+                }
+                else if (i % 3 == 0)
+                {
+                    totalPlunder += (dailyPlunder * 0.50);
+                }
+                else if (i % 5 == 0)
+                {
+                    totalPlunder -= (totalPlunder * 0.30);
+                }
+            }
+
+            if (totalPlunder >= expectedPlunder)
+            {
+                Console.WriteLine($"Ahoy! {totalPlunder:f2} plunder gained.");
+            }
+            else
+            {
+                double percentage = (totalPlunder / expectedPlunder) * 100;
+                Console.WriteLine($"Collected only {percentage:f2}% of the plunder.");
+            }
         }
     }
 }
