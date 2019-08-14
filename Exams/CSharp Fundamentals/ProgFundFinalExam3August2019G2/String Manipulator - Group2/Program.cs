538bc026d2b2b8a961f31c6f7815549c0ee25fe5
erayridvan <52295311+erayridvan@users.noreply.github.com>
2019-08-14 08:15:58 +0100

    C# Fundamentals

---
+++ Exams/CSharp Fundamentals/ProgFundFinalExam3August2019G2/String Manipulator - Group2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace String_Manipulator___Group2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
