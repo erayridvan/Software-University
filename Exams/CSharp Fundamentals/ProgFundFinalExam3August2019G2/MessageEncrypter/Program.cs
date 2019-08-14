b187664a4069e1770b3e2fb441b7a851972333f7
erayridvan <52295311+erayridvan@users.noreply.github.com>
2019-08-14 08:20:56 +0100

    C# Fundamentals

--- Exams/CSharp Fundamentals/ProgFundFinalExam3August2019G2/MessageEncrypter/Program.cs
+++ Exams/CSharp Fundamentals/ProgFundFinalExam3August2019G2/MessageEncrypter/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace MessageEncrypter
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            int times = int.Parse(Console.ReadLine());
+            var pattern = new Regex(@"(\*|@)(?<name>[A-Z][a-z]{2,})\1: \[(?<firstLetter>[A-za-z])\]\|\[(?<secondLetter>[A-Za-z])\]\|\[(?<thirdLetter>[A-Za-z])\]\|$");
+
+            for (int i = 0; i < times; i++)
+            {
+                string text = Console.ReadLine();
+
+                var matches = pattern.Match(text);
+
+
+                if (matches.Success)
+                {
+                    string name = matches.Groups["name"].Value;
+                    char letter = char.Parse(matches.Groups["firstLetter"].Value);
+                    char letter1 = char.Parse(matches.Groups["secondLetter"].Value);
+                    char letter2 = char.Parse(matches.Groups["thirdLetter"].Value);
+
+                    Console.WriteLine($"{name}: {(int)letter} {(int)letter1} {(int)letter2}");
+
+                }
+                else
+                {
+                    Console.WriteLine("Valid message not found!");
+                }
+
+            }
         }
     }
 }
