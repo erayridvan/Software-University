0f4036861f4740211e9f2f0459497f4dfb2ddff7
erayridvan <52295311+erayridvan@users.noreply.github.com>
2019-08-16 07:55:49 +0100

    C# Fundamentals

---
+++ Exams/CSharp Fundamentals/ProgFundFinalExam21July2019/EmojiSumator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmojiSumator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
