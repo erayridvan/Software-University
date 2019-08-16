cf0cada872fc06241fd93792f872a33b5bc04918
erayridvan <52295311+erayridvan@users.noreply.github.com>
2019-08-16 07:57:06 +0100

    C# Fundamentals

---
+++ Exams/CSharp Fundamentals/ProgFundFinalExam21July2019/FeedTheAnimals/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FeedTheAnimals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
