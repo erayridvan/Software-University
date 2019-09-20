a803111a32aca0c7d554e8e0306e8e93b375686d
erayridvan <52295311+erayridvan@users.noreply.github.com>
2019-09-20 12:02:55 +0300

    C# Advanced

---
+++ C# Advanced/CSharpAdvancedStacksAndQueuesExercises/BasicStackOperations/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BasicStackOperations
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] args = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int elementsToPop = args[1];
+            int elementContain = args[2];
+
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> nums = new Stack<int>(numbers);
+
+            for (int i = 0; i < elementsToPop; i++)
+            {
+                if (nums.Count>=1)
+                {
+                    nums.Pop();
+                }
+            }
+
+            if (nums.Contains(elementContain))
+            {
+                Console.WriteLine("true");
+            }
+            else if (nums.Count>1 && !(nums.Contains(elementContain)))
+            {
+                Console.WriteLine(nums.Min());
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
---
+++ C# Advanced/CSharpAdvancedStacksAndQueuesLab/TrafficJam/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TrafficJam
+{
+    class Program
+    {
+        static void Main()
+        {
+            Queue<string> cars = new Queue<string>();
+            string currCar = string.Empty;
+            int index = int.Parse(Console.ReadLine());
+            int counter = 0;
+
+            while ((currCar = Console.ReadLine()) != "end")
+            {
+                if (currCar=="green")
+                {
+                    if (index<=cars.Count)
+                    {
+                        for (int i = 0; i < index; i++)
+                        {
+                            var currentCar = cars.Dequeue();
+                            Console.WriteLine($"{currentCar} passed!");
+                            counter++;
+                        }
+                    }
+                    else
+                    {
+                        for (int i = 0; i < cars.Count; i++)
+                        {
+                            var currentCar = cars.Dequeue();
+                            Console.WriteLine($"{currentCar} passed!");
+                            counter++;
+                            i = -1;
+                        }
+                    }
+                   
+                }
+                else
+                {
+                    cars.Enqueue(currCar);
+                }
+            }
+
+            Console.WriteLine($"{counter} cars passed the crossroads.");
+        }
+    }
+}
