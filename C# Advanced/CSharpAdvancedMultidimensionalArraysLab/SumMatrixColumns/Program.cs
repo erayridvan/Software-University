8ba0e7f9c0a069928412f2e740c3641ed8a6e0ff
erayridvan <52295311+erayridvan@users.noreply.github.com>
2019-09-22 11:53:38 +0300

    C# Advanced

--- C# Advanced/CSharpAdvancedMultidimensionalArraysLab/SumMatrixColumns/Program.cs
+++ C# Advanced/CSharpAdvancedMultidimensionalArraysLab/SumMatrixColumns/Program.cs
@@ -4,9 +4,33 @@ namespace SumMatrixColumns
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var dimentions = Console.ReadLine().Split(", ");
+            var rows = int.Parse(dimentions[0]);
+            var cols = int.Parse(dimentions[1]);
+            
+            var matrix = new int[rows, cols];
+            for (int row = 0; row < rows; row++)
+            {
+                var rowAsString = Console.ReadLine();
+                var elements = rowAsString.Split(" ");
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = int.Parse(elements[col]);
+                }
+            }
+
+
+            for (int col = 0; col < cols; col++)
+            {
+                int sum = 0;
+                for (int row = 0; row < rows; row++)
+                {
+                    sum += matrix[row, col];
+                }
+                Console.WriteLine(sum);
+            }
         }
     }
 }
