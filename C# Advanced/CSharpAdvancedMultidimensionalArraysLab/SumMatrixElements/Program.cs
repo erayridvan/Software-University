e4bf0a5e3d373b307906c0e4982d221ce792445b
erayridvan <52295311+erayridvan@users.noreply.github.com>
2019-09-22 11:35:24 +0300

    C# Advanced

---
+++ C# Advanced/CSharpAdvancedMultidimensionalArraysLab/SumMatrixElements/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SumMatrixElements
+{
+    class Program
+    {
+        static void Main()
+        {
+            var dimentions = Console.ReadLine().Split(", ");
+            var rows = int.Parse(dimentions[0]);
+            var cols = int.Parse(dimentions[1]);
+            long sum =0;
+
+            var matrix = new int[rows, cols];
+            for (int row = 0; row < rows; row++)
+            {
+                var rowAsString = Console.ReadLine();
+                var elements = rowAsString.Split(", ");
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row,col] = int.Parse(elements[col]);
+                    sum += matrix[row, col];
+                }
+            }
+
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(matrix.GetLength(1));
+            Console.WriteLine(sum);
+        }
+    }
+}
