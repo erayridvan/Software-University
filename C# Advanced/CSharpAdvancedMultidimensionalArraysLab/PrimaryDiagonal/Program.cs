f15c92a3d886caf5a448cabb897db2806e299122
erayridvan <52295311+erayridvan@users.noreply.github.com>
2019-09-23 11:44:23 +0300

    C# Advanced

--- C# Advanced/CSharpAdvancedMultidimensionalArraysLab/PrimaryDiagonal/Program.cs
+++ C# Advanced/CSharpAdvancedMultidimensionalArraysLab/PrimaryDiagonal/Program.cs
@@ -6,7 +6,27 @@ namespace PrimaryDiagonal
     {
         static void Main()
         {
+            int matrixSize = int.Parse(Console.ReadLine());
 
+            var matrix = new int[matrixSize, matrixSize];
+
+            for (int row = 0; row < matrixSize; row++)
+            {
+                var rowAsString = Console.ReadLine();
+                var elements = rowAsString.Split(" ");
+                for (int col = 0; col < matrixSize; col++)
+                {
+                    matrix[row, col] = int.Parse(elements[col]);
+                }
+            }
+
+            int diagonalSum = 0;
+            for (int i = 0; i < matrixSize; i++)
+            {
+                diagonalSum += matrix[i,i];
+            }
+
+            Console.WriteLine(diagonalSum);
         }
     }
 }
---
+++ C# Advanced/CSharpAdvancedMultidimensionalArraysLab/SymbolnMatrix/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SymbolnMatrix
+{
+    class Program
+    {
+        static void Main()
+        {
+            int matrixSize = int.Parse(Console.ReadLine());
+
+            var matrix = new char[matrixSize, matrixSize];
+
+            for (int row = 0; row < matrixSize; row++)
+            {
+                var rowAsString = Console.ReadLine();
+                //var elements = rowAsString.Split(" ");
+                for (int col = 0; col < matrixSize; col++)
+                {
+                    matrix[row, col] = rowAsString[col];
+                }
+            }
+
+            char symbolToCheck = char.Parse(Console.ReadLine());
+
+            for (int row = 0; row < matrixSize; row++)
+            {
+                for (int col = 0; col < matrixSize; col++)
+                {
+                    char currentSymbol = matrix[row, col];
+
+                    if (currentSymbol==symbolToCheck)
+                    {
+                        Console.WriteLine($"({row}, {col})");
+                        return;
+                    }
+                }
+            }
+
+            Console.WriteLine($"{symbolToCheck} does not occur in the matrix");
+        }
+    }
+}
