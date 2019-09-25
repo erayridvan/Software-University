3010cde7ecd45e621d393e59456aede708798395
erayridvan <52295311+erayridvan@users.noreply.github.com>
2019-09-26 00:05:51 +0300

    C# Advanced

---
+++ C# Advanced/CSharpAdvancedMultidimensionalArraysExercises/DiagonalDifference/Program.cs
@@ -0,0 +1,41 @@
+﻿    using System;
+
+    namespace DiagonalDifference
+    {
+        class Program
+        {
+            static void Main()
+            {
+                int n = int.Parse(Console.ReadLine());
+                var matrix = new int[n,n];  
+
+                for (int i = 0; i < n; i++)
+                {
+                    string[] rowAsString = Console.ReadLine().Split();
+                    for (int k = 0; k < rowAsString.Length; k++)
+                    {
+                        matrix[i,k] = int.Parse(rowAsString[k]);
+                    }
+                }
+
+                int primaryDiagonal = 0;
+
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    primaryDiagonal += matrix[i, i];
+                }
+
+                int secondaryDiagonal = 0;
+
+                for (int i = matrix.GetLength(0) - 1; i >= 0; i--)
+                {
+
+                        secondaryDiagonal += matrix[i, Math.Abs(n-1-i)];
+                }
+
+                int absDiff = Math.Abs(primaryDiagonal - secondaryDiagonal);
+
+                Console.WriteLine(absDiff);
+            }
+        }
+    }
---
+++ C# Advanced/CSharpAdvancedMultidimensionalArraysExercises/SquaresInMatrix/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace SquaresInMatrix
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] dimensions = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = dimensions[0];
+            int cols = dimensions[1];
+
+            var matrix = new char[rows,cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                char[] letters = Console.ReadLine()
+                .Split(" ")
+                .Select(char.Parse)
+                .ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = letters[col]; ;
+                }
+            }
+
+            int counter = 0;
+            for (int row = 0; row < rows-1; row++)
+            {
+                for (int col = 0; col < cols-1; col++)
+                {
+                    if (matrix[row,col]==matrix[row,col+1] &&
+                        matrix[row+1,col] == matrix[row+1,col+1] &&
+                        matrix[row, col] == matrix[row+1, col + 1])
+                    {
+                        counter++;
+                    }
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
