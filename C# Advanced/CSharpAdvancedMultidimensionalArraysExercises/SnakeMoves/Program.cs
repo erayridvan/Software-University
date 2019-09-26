6bbdfe28a73a644f540be25df990c13fab241a89
erayridvan <52295311+erayridvan@users.noreply.github.com>
2019-09-27 02:19:19 +0300

    C# Advanced

--- C# Advanced/CSharpAdvancedMultidimensionalArraysExercises/SnakeMoves/Program.cs
+++ C# Advanced/CSharpAdvancedMultidimensionalArraysExercises/SnakeMoves/Program.cs
@@ -1,12 +1,59 @@
 ﻿using System;
+using System.Linq;
 
 namespace SnakeMoves
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            int[] dimensions = Console.ReadLine()
+              .Split(" ")
+              .Select(int.Parse)
+              .ToArray();
+
+            int rows = dimensions[0];
+            int cols = dimensions[1];
+
+            var matrix = new char[rows, cols];
+
+            string snake = Console.ReadLine();
+            int counter = 0;
+            for (int row = 0; row < rows; row++)
+            {
+                counter++;
+
+                if (counter%2==0)
+                {
+                    for (int col = cols - 1; col >= 0; col--)
+                    {
+                        char currentSybol = snake[0];
+                        matrix[row, col] = currentSybol;
+                        snake = snake.Remove(0, 1);
+                        snake += currentSybol;
+                    }
+                }
+                else
+                {
+                    for (int col = 0; col < cols; col++)
+                    {
+                        char currentSybol = snake[0];
+                        matrix[row, col] = currentSybol;
+                        snake = snake.Remove(0, 1);
+                        snake += currentSybol;
+                    }
+                }
+                
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    Console.Write($"{matrix[row,col]} ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
