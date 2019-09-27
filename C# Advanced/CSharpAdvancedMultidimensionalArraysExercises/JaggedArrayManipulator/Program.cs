fdaec0effd1497f673a28422261f82696acbec0b
erayridvan <52295311+erayridvan@users.noreply.github.com>
2019-09-28 00:57:34 +0300

    C# Advanced

---
+++ C# Advanced/CSharpAdvancedMultidimensionalArraysExercises/JaggedArrayManipulator/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Linq;
+
+namespace JaggedArrayManipulator
+{
+    class Program
+    {
+        static void Main()
+        {
+            int rows = int.Parse(Console.ReadLine());
+
+            var matrix = new double[rows][];
+
+            InitializationMatrix(matrix, rows);
+            AnalyzingMatrix(matrix, rows);
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "End")
+                {
+                    break;
+                }
+
+                string[] args = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string operation = args[0];
+                int row = int.Parse(args[1]);
+                int col = int.Parse(args[2]);
+                int value = int.Parse(args[3]);
+
+
+                if (row >= 0 && row < matrix.Length && col >= 0 && col< matrix[row].Length)
+                {
+                    if (operation == "Add")
+                    {
+                        matrix[row][col] += value;
+                    }
+                    else if (operation == "Subtract")
+                    {
+                        matrix[row][col] -= value;
+                    }
+                }
+            }
+
+            foreach (var numbers in matrix)
+            {
+                Console.WriteLine($"{string.Join(" ", numbers)} ");
+            }
+        }
+
+        private static void AnalyzingMatrix(double[][] matrix, int rows)
+        {
+            for (int row = 0; row < rows - 1; row++)
+            {
+                if (matrix[row].GetLength(0) == matrix[row + 1].GetLength(0))
+                {
+                    for (int i = 0; i < matrix[row].GetLength(0); i++)
+                    {
+                        matrix[row][i] *= 2;
+                        matrix[row + 1][i] *= 2;
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < matrix[row].GetLength(0); i++)
+                    {
+                        matrix[row][i] /= 2;
+                    }
+
+                    for (int i = 0; i < matrix[row + 1].GetLength(0); i++)
+                    {
+                        matrix[row + 1][i] /= 2;
+                    }
+                }
+            }
+        }
+
+        private static void InitializationMatrix(double[][] matrix, int rows)
+        {
+            for (int row = 0; row < rows; row++)
+            {
+                double[] numbers = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(double.Parse)
+                    .ToArray();
+
+                matrix[row] = numbers;
+            }
+        }
+    }
+}
---
+++ C# Advanced/CSharpAdvancedMultidimensionalArraysExercises/KnightGame/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Linq;
+
+namespace KnightGame
+{
+    class Program
+    {
+        static void Main()
+        {
+            int dimentions = int.Parse(Console.ReadLine());
+
+            var gameBoard = new string[dimentions, dimentions];
+
+            InitializationGameBoard(gameBoard, dimentions);
+
+            int knightCounter = 0;
+            if (dimentions > 2)
+            {
+                Console.WriteLine(KnightRemoving(gameBoard, dimentions, knightCounter));
+            }
+            else
+            {
+                Console.WriteLine(knightCounter);
+            }
+        }
+
+        private static int KnightRemoving(string[,] gameBoard, int dimentions, int knightCounter)
+        {
+            for (int row = 0; row < dimentions; row++)
+            {
+                for (int col = 0; col < dimentions; col++)
+                {
+                    if (gameBoard[row, col] == "K")
+                    {
+                        if (col + 1 < gameBoard.GetLength(1) && row + 2 < gameBoard.GetLength(0))
+                        {
+                            if (gameBoard[row + 2, col + 1] == "K")
+                            {
+                                gameBoard[row + 2, col + 1] = "0";
+                                knightCounter++;
+                            }
+                        }
+
+                        if (col + 2 < gameBoard.GetLength(1) && row + 1 < gameBoard.GetLength(0))
+                        {
+                            if (gameBoard[row + 1, col + 2] == "K")
+                            {
+                                gameBoard[row + 1, col + 2] = "0";
+                                knightCounter++;
+                            }
+                        }
+
+                        if (col - 1 >= 0 && col - 1 < gameBoard.GetLength(1) && row + 2 < gameBoard.GetLength(0))
+                        {
+                            if (gameBoard[row + 2, col - 1] == "K")
+                            {
+                                gameBoard[row + 2, col - 1] = "0";
+                                knightCounter++;
+                            }
+                        }
+
+                        if (col - 2 >= 0 && col - 2 < gameBoard.GetLength(1) && row + 1 < gameBoard.GetLength(0))
+                        {
+                            if (gameBoard[row + 1, col - 2] == "K")
+                            {
+                                gameBoard[row + 1, col - 2] = "0";
+                                knightCounter++;
+                            }
+                        }
+
+
+
+                        if (col - 1 >= 0 && row - 2 >= 0 && col - 1 < gameBoard.GetLength(1) && row - 2 < gameBoard.GetLength(0))
+                        {
+                            if (gameBoard[row - 2, col - 1] == "K")
+                            {
+                                gameBoard[row - 2, col - 1] = "0";
+                                knightCounter++;
+                            }
+                        }
+
+                        if (row - 2 >= 0 && col + 1 < gameBoard.GetLength(1) && row - 2 < gameBoard.GetLength(0))
+                        {
+                            if (gameBoard[row - 2, col + 1] == "K")
+                            {
+                                gameBoard[row - 2, col + 1] = "0";
+                                knightCounter++;
+                            }
+                        }
+
+                        if (col - 2 >= 0 && row - 1 >= 0 && col - 2 < gameBoard.GetLength(1) && row - 1 < gameBoard.GetLength(0))
+                        {
+                            if (gameBoard[row - 1, col - 2] == "K")
+                            {
+                                gameBoard[row - 1, col - 2] = "0";
+                                knightCounter++;
+                            }
+                        }
+
+                        if (row - 1 >= 0 && col + 2 < gameBoard.GetLength(1) && row - 1 < gameBoard.GetLength(0))
+                        {
+                            if (gameBoard[row - 1, col + 2] == "K")
+                            {
+                                gameBoard[row - 1, col + 2] = "0";
+                                knightCounter++;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return knightCounter;
+        }
+
+        private static void InitializationGameBoard(string[,] gameBoard, int dimentions)
+        {
+            for (int row = 0; row < dimentions; row++)
+            {
+                string input = Console.ReadLine().Trim();
+                string[] arrSymbols = input.Split(' ',StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                for (int col = 0; col < dimentions; col++)
+                {
+                    gameBoard[row, col] = arrSymbols[col];
+                }
+            }
+        }
+    }
+}
