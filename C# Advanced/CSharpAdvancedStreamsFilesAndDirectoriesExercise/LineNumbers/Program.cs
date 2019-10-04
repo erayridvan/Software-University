b410d10e6c6597211a83bdcf56a737ac367bad62
erayridvan <52295311+erayridvan@users.noreply.github.com>
2019-10-04 13:21:44 +0300

    C# Advanced

--- C# Advanced/CSharpAdvancedStreamsFilesAndDirectoriesExercise/LineNumbers/Program.cs
+++ C# Advanced/CSharpAdvancedStreamsFilesAndDirectoriesExercise/LineNumbers/Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
-namespace LineNumbers
+class LineNumbers
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        using(StreamReader reader = new StreamReader("text.txt"))
         {
-            Console.WriteLine("Hello World!");
+            StringBuilder sb = new StringBuilder();
+            int lineCounter = 1;
+
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+
+                int punCounter = 0;
+                int letterCounter = 0;
+
+                for (int i = 0; i < line.Length; i++)
+                {
+                    char symbol = line[i];
+                    
+                    if (symbol=='-' || symbol == '?' || symbol == '.' 
+                        || symbol == '!' || symbol == '\'' || symbol == ',')
+                    {
+                        punCounter++;
+                    }
+                    else
+                    {
+                        if (symbol!=' ')
+                        {
+                            letterCounter++;
+                        }
+                    }
+                }
+
+                sb.AppendLine("Line "+lineCounter +": "+ line+"("+letterCounter+")" + "(" + punCounter + ")");
+
+                lineCounter++;
+            }
+
+            File.WriteAllText("output.txt", sb.ToString());
         }
     }
 }
