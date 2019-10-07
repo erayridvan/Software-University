8345fe75ba6654872978e03e6bc53c8236d66990
erayridvan <52295311+erayridvan@users.noreply.github.com>
2019-10-07 15:06:22 +0300

    C# Advanced

--- C# Advanced/CSharpAdvancedFunctionalProgrammingExercises/CustomMinFunction/Program.cs
+++ C# Advanced/CSharpAdvancedFunctionalProgrammingExercises/CustomMinFunction/Program.cs
@@ -8,13 +8,29 @@ namespace CustomMinFunction
     {
         static void Main()
         {
+            Func<int[], int> minFunc = inputNumbers =>
+            {
+                int minValue = int.MaxValue;
+
+                foreach (var currentNumber in inputNumbers)
+                {
+                    if (currentNumber<minValue)
+                    {
+                        minValue = currentNumber;
+                    }
+                }
+
+                return minValue;
+            };
+
             int[] numbers = Console.ReadLine()
                 .Split(" ")
                 .Select(int.Parse)
                 .ToArray();
 
-            int minValue = numbers.Min();
-            Console.WriteLine(minValue);
+            int minNumber = minFunc(numbers);
+
+            Console.WriteLine(minNumber);
         }
     }
 }
--- C# Advanced/CSharpAdvancedFunctionalProgrammingExercises/FindEvensOrOdds/Program.cs
+++ C# Advanced/CSharpAdvancedFunctionalProgrammingExercises/FindEvensOrOdds/Program.cs
@@ -1,12 +1,64 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace FindEvensOrOdds
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            int[] boundaries = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+            int lowerBound = boundaries[0];
+            int upperBound = boundaries[1];
+
+            string command = Console.ReadLine();
+
+            Predicate<int> odd = FindOdd;
+            Predicate<int> Even = FindEven;
+
+            List<int> numbers = new List<int>();
+            for (int i = lowerBound; i <= upperBound; i++)
+            {
+                if (command == "odd")
+                {
+                    if (odd(i))
+                    {
+                        numbers.Add(i);
+                    }
+                }
+                else if (command == "even")
+                {
+                    if (Even(i))
+                    {
+                        numbers.Add(i);
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ",numbers));
+        }
+
+        private static bool FindOdd(int number)
+        {
+            if (number % 2 == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static bool FindEven(int number)
+        {
+            if (number%2==0)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
