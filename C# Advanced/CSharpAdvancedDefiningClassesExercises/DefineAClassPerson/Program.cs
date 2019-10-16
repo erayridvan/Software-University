b1a2044107bcaaffd9d5698c202b0c22252e38fe
erayridvan <52295311+erayridvan@users.noreply.github.com>
2019-10-16 21:49:27 +0300

    C# Advanced

---
+++ C# Advanced/CSharpAdvancedDefiningClassesExercises/DefineAClassPerson/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DefineAClassPerson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- C# Advanced/CSharpAdvancedDefiningClassesLab/Car/Program.cs
+++ C# Advanced/CSharpAdvancedDefiningClassesLab/Car/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 
-namespace Car
+namespace CarManufacturer
 {
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+    }
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Car car = new Car();
+            car.Make = "VW";
+            car.Model = "MK3";
+            car.Year = 1992;
+
+            Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}");
         }
     }
 }
