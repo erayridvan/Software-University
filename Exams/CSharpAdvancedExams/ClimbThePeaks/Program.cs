4e40f0cd60eed7b97e3a3987a87109a0c6a9d039
erayridvan <52295311+erayridvan@users.noreply.github.com>
2023-04-09 08:07:41 +0300

    Exam Preparation

---
+++ Exams/CSharpAdvancedExams/ClimbThePeaks/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ClimbThePeaks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> food = new Stack<int>
+                (Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray());
+
+            Queue<int> stamina = new Queue<int>
+                (Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray());
+
+            Dictionary<string, int> peaksInfo =new Dictionary<string, int>()
+            {
+                { "Vihren",80 },
+                { "Kutelo",90 },
+                { "Banski Suhodol",100 },
+                { "Polezhan",60 },
+                { "Kamenitza",70 }
+            };
+
+            bool isClimbed = false;
+
+            for (int i = 0; i < length; i++)
+            {
+
+            }
+    }
+}
+}
